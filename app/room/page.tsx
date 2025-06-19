3636f4eb6667ad9f264c98066a2e4a21e3ee3b17
mumer2 <umermalik8280@gmail.com>
2025-06-19 07:42:13 +0500

    NEW

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -32,4 +32,3 @@ export default function RoomPage() {
     </LiveKitRoom>
   );
 }
-
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
     serverActions: false,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
